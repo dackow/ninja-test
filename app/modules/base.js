af0ce6a03bb420fe6a8f6b8a921923199347d1e3
dackow <dackow@gmail.com>
2015-05-06 22:28:32 +0200

    modified:   app/modules/base.js modified:   app/modules/posts/postModule.js

--- app/modules/base.js
+++ app/modules/base.js
@@ -2,23 +2,48 @@
 
 var postManBase = angular.module('PostMan.base', ['ui.router']);
 
-var postManBaseFactory = postManBase.factory('Base', function(){
-	var baseFunctionality = {};
-
-	baseFunctionality.populateStates = function(states){
-		states.forEach(function(item){
+/*var postManBaseValue = angular.value('BaseValue', {
+	populateStates : function(states){
+				states.forEach(function(item){
 			$stateProvider.state(item.name, {
 				url: item.url,
 				templateUrl: item.templateUrl,
 				controller: item.controller
 			});
 		});
-	};
-
-	return {
-		BaseFunctionality: baseFunctionality
 	}
+	});
+*/
+
+var postManBaseFactory = postManBase.factory('MyFactory', function(){
+	var _service = {};
+
+	_service.populateStates = function(states){
+		states.forEach(function(item){
+			console.log(item|json);
+		});
+	}
+
+	return _service;
 });
+
+// var postManBaseFactory = postManBase.factory('Base', function(){
+// 	var baseFunctionality = {};
+
+// 	baseFunctionality.populateStates = function(states){
+// 		states.forEach(function(item){
+// 			$stateProvider.state(item.name, {
+// 				url: item.url,
+// 				templateUrl: item.templateUrl,
+// 				controller: item.controller
+// 			});
+// 		});
+// 	};
+
+// 	return {
+// 		BaseFunctionality: baseFunctionality
+// 	}
+// });
 
 
 
--- app/modules/posts/postModule.js
+++ app/modules/posts/postModule.js
@@ -1,6 +1,6 @@
-var postManModule = angular.module('PostMan.posts', ['PostMan.posts.controllers','PostMan.base','ui.router']);
+var postManModule = angular.module('PostMan.posts', ['PostMan.posts.controllers','ui.router']);
 
-postManModule.config(['$stateProvider','$locationProvider', 'BaseProvider', function ($stateProvider,$locationProvider, PostController, PostDetailsController, BaseProvider) {
+postManModule.config(['$stateProvider','$locationProvider',function ($stateProvider,$locationProvider, PostController, PostDetailsController) {
 	var states = [
 	{
 		name: 'allPosts',
@@ -16,18 +16,19 @@ postManModule.config(['$stateProvider','$locationProvider', 'BaseProvider', func
 	},
 	];
 
+	// BaseValue.populateStates(states);
 	// var injector = angular.injector(['PostMan.base']);
 	// var base = injector.get('BaseFunctionality');
 	// base.BaseFunctionality.populateStates(states, $stateProvider);
 
 	//BaseProvider.BaseFunctionality(states);
-	// states.forEach(function(item){
-	// 	$stateProvider.state(item.name, {
-	// 		url: item.url,
-	// 		templateUrl: item.templateUrl,
-	// 		controller: item.controller
-	// 	})
-	// });
+	states.forEach(function(item){
+		$stateProvider.state(item.name, {
+			url: item.url,
+			templateUrl: item.templateUrl,
+			controller: item.controller
+		})
+	});
 
 
 	// $stateProvider.state('allPosts', {
